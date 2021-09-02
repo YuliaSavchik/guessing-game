3cf0183f485aa6a1d5036da44c4d9b85abe24f68
YS <yulia.deamchik@gmail.com>
2021-09-02 16:50:08 +0300

    init: add answer task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.number = Math.ceil(this.max / 2)
+        this.min
+        this.max
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.number = Math.ceil((this.max - this.min) / 2 + this.min)
+        return this.number
     }
 
     lower() {
-
+        this.setRange(this.min, this.number)
     }
 
     greater() {
-
+        this.setRange(this.number, this.max)
     }
+    
 }
 
 module.exports = GuessingGame;
